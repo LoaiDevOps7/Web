7a486d8952295cf277f018565dc546d778a0767d
LoaiDevOps7 <loaidevop.work@gmail.com>
2025-03-30 20:42:03 +0300

    3-30

--- src/api/message.ts
+++ src/api/message.ts
@@ -1,94 +1,94 @@
-import { Conversation, Message, Reaction } from "@/types/Message";
-import axiosClient from "../lib/axiosClient";
+// import { Conversation, Message, Reaction } from "@/types/Message";
+// import axiosClient from "../lib/axiosClient";
 
 
-export const CreateConversation = async (
-  receiverId: number,
-  content: string,
-  isImportant?: boolean
-) => {
-  const response = await axiosClient.post("/chat/conversations", {
-    receiverId,
-    content,
-    isImportant,
-  });
-  return response.data;
-};
-
-export const GetConversations = async () => {
-  const response = await axiosClient.get("/chat/conversations");
-  return response.data;
-};
-
-export const GetMessages = async (
-  conversationId: string
-) => {
-  const response = await axiosClient.get(`/chat/messages/${conversationId}`);
-  return response.data;
-};
-
-// export const SendMessage = async (data: {
-//   conversationId: string;
-//   content?: string;
-//   receiverId: number;
-// }): Promise<Message> => {
-//   const response = await axiosClient.post("/chat/messages", data);
+// export const CreateConversation = async (
+//   receiverId: number,
+//   content: string,
+//   isImportant?: boolean
+// ) => {
+//   const response = await axiosClient.post("/chat/conversations", {
+//     receiverId,
+//     content,
+//     isImportant,
+//   });
 //   return response.data;
 // };
 
-export const UploadFile = async (file: File, userId: number): Promise<{ fileUrl: string }> => {
-  const formData = new FormData();
-  formData.append("file", file);
+// export const GetConversations = async () => {
+//   const response = await axiosClient.get("/chat/conversations");
+//   return response.data;
+// };
 
-  const response = await axiosClient.post(`/chat/upload/${userId}`, formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+// export const GetMessages = async (
+//   conversationId: string
+// ) => {
+//   const response = await axiosClient.get(`/chat/messages/${conversationId}`);
+//   return response.data;
+// };
 
-  return response.data;
-};
+// // export const SendMessage = async (data: {
+// //   conversationId: string;
+// //   content?: string;
+// //   receiverId: number;
+// // }): Promise<Message> => {
+// //   const response = await axiosClient.post("/chat/messages", data);
+// //   return response.data;
+// // };
 
-export const SearchMessages = async (
-  conversationId: string,
-  query: string
-): Promise<Message[]> => {
-  const response = await axiosClient.get(
-    `/chat/search?conversationId=${conversationId}&q=${encodeURIComponent(
-      query
-    )}`
-  );
-  return response.data;
-};
+// export const UploadFile = async (file: File, userId: number): Promise<{ fileUrl: string }> => {
+//   const formData = new FormData();
+//   formData.append("file", file);
 
-export const EditMessage = async (
-  messageId: string,
-  content: string
-): Promise<Message> => {
-  const response = await axiosClient.post(`/chat/edit/${messageId}`, {
-    content,
-    userId: JSON.parse(localStorage.getItem("user") || "{}").id,
-  });
-  return response.data;
-};
+//   const response = await axiosClient.post(`/chat/upload/${userId}`, formData, {
+//     headers: {
+//       "Content-Type": "multipart/form-data",
+//     },
+//   });
 
-export const DeleteMessage = async (
-  messageId: string
-): Promise<{ message: string }> => {
-  const response = await axiosClient.post(`/chat/delete/${messageId}`, {
-    userId: JSON.parse(localStorage.getItem("user") || "{}").id,
-  });
-  return response.data;
-};
+//   return response.data;
+// };
 
-export const AddReaction = async (
-  messageId: string,
-  reactionType: string
-): Promise<Reaction> => {
-  const response = await axiosClient.post("/chat/react", {
-    messageId,
-    userId: JSON.parse(localStorage.getItem("user") || "{}").id,
-    reactionType,
-  });
-  return response.data;
-};
+// export const SearchMessages = async (
+//   conversationId: string,
+//   query: string
+// ): Promise<Message[]> => {
+//   const response = await axiosClient.get(
+//     `/chat/search?conversationId=${conversationId}&q=${encodeURIComponent(
+//       query
+//     )}`
+//   );
+//   return response.data;
+// };
+
+// export const EditMessage = async (
+//   messageId: string,
+//   content: string
+// ): Promise<Message> => {
+//   const response = await axiosClient.post(`/chat/edit/${messageId}`, {
+//     content,
+//     userId: JSON.parse(localStorage.getItem("user") || "{}").id,
+//   });
+//   return response.data;
+// };
+
+// export const DeleteMessage = async (
+//   messageId: string
+// ): Promise<{ message: string }> => {
+//   const response = await axiosClient.post(`/chat/delete/${messageId}`, {
+//     userId: JSON.parse(localStorage.getItem("user") || "{}").id,
+//   });
+//   return response.data;
+// };
+
+// export const AddReaction = async (
+//   messageId: string,
+//   reactionType: string
+// ): Promise<Reaction> => {
+//   const response = await axiosClient.post("/chat/react", {
+//     messageId,
+//     userId: JSON.parse(localStorage.getItem("user") || "{}").id,
+//     reactionType,
+//   });
+//   return response.data;
+// };
